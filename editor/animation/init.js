16552845882b0be3db4bade563db0ad666daedf2
Alexander Blaschuk <blaschuk95@gmail.com>
2022-07-29 12:15:48 +0300

    new

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -1,17 +1,8 @@
 //Dont change it
 //Dont change it
-requirejs(['ext_editor_io', 'jquery_190'],
+requirejs(['ext_editor_io2', 'jquery_190'],
     function (extIO, $) {
-        
-        var $tryit;
-
-        var io = new extIO({
-            multipleArguments: true,
-            functions: {
-                python: 'replace_last',
-                js: 'replaceLast'
-            }
-        });
+        var io = new extIO({});
         io.start();
     }
 );
